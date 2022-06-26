baf02089ea2695c95b7a2715714c3e6f1ba929af
lnicepei <dmitrii.shaidorov@gmail.com>
2022-06-26 11:47:11 +0300

    Get forecast for the upcoming seven days

--- src/getWeather.js
+++ src/getWeather.js
@@ -4,33 +4,27 @@ async function getWeather() {
   const cityInput = document.querySelector("input");
   let unfecthedWeatherData, unfetchedForecast;
   try {
-    if (cityInput.value) {
       unfecthedWeatherData = await fetch(
         `http://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&APPID=9d2f4030cf37634f60b9012e5bd611be`,
         { mode: "cors" }
       );
+
+      
+      const weatherData = await unfecthedWeatherData.json();
+      
+      
       unfetchedForecast = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?q=${cityInput.value}&appid=9d2f4030cf37634f60b9012e5bd611be`,
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&exclude=current,hourly,minutely,alerts&units=metric&appid=9d2f4030cf37634f60b9012e5bd611be`,
         { mode: "cors" }
       );
-    } else {
-      unfecthedWeatherData = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=London&APPID=9d2f4030cf37634f60b9012e5bd611be`,
-        { mode: "cors" }
-      );
-      unfetchedForecast = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?q=London&appid=9d2f4030cf37634f60b9012e5bd611be`,
-        { mode: "cors" }
-      );
-    }
 
-    const weatherData = await unfecthedWeatherData.json();
+    const forecastData = await unfetchedForecast.json();
 
-    const forecast = await unfetchedForecast.json();
-    console.log(weatherData);
-    console.log(forecast);
 
-    createCurrentWeather(weatherData);
+    // console.log(weatherData, forecastData);
+    // console.log(forecastData);
+
+    createCurrentWeather(weatherData, forecastData);
   } catch (error) {
     console.log(error);
     alert("Please enter an existing city");
