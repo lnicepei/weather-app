e492c1b7069065dabf02f5e6116cfb69ddce648d
lnicepei <dmitrii.shaidorov@gmail.com>
2022-06-26 18:03:54 +0300

    Remove IIFE

--- src/index.js
+++ src/index.js
@@ -3,7 +3,3 @@ import { getWeather } from "./getWeather";
 document.addEventListener("keypress", (e) => {
   if (e.key === "Enter") getWeather();
 });
-
-// (function startingWeather() {
-//   getWeather();
-// })();
