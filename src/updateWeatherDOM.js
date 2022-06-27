fd2235d28a3ae2947e3dfa03b1da59c1ac379d5b
lnicepei <dmitrii.shaidorov@gmail.com>
2022-06-27 12:22:35 +0300

    Separate code into functions

--- src/updateWeatherDOM.js
+++ src/updateWeatherDOM.js
@@ -3,33 +3,12 @@ import { format } from "date-fns";
 function createCurrentWeather(weatherData, forecastData) {
   let currentDegrees = "°C";
 
-  // const todayTemperature = document.querySelector(".today__temperature");
-  // const todayCity = document.querySelector(".today__city");
-  // const todayImage = document.querySelector(".today__image");
-  const toggleButton = document.querySelector(".toggle-temp");
+  const toggleButton = document.querySelector(".search__toggle");
   const forecastDays = [...document.querySelectorAll(".forecast__card")];
-  // const todayItsNow = document.querySelector(".additional__its-now");
-  // const todayHumitidy = document.querySelector(".additional__humidity");
-  // const todayWindSpeed = document.querySelector(".additional__wind-speed");
 
   removeWeatherClassName();
 
   updateTodayWeather(weatherData, currentDegrees);
-
-  // const weatherImage = document.createElement("img");
-  // weatherImage.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-  // todayImage.textContent = "";
-  // todayImage.appendChild(weatherImage);
-
-  // todayItsNow.textContent = `${weatherData.weather[0].description} now in ${weatherData.name}`;
-  // todayCity.textContent = `${weatherData.name}, ${weatherData.sys.country}`;
-  // todayHumitidy.textContent = `Humidity: ${weatherData.main.humidity}%`;
-  // todayWindSpeed.textContent = `Wind speed: ${weatherData.wind.speed} m/s`;
-
-  // todayTemperature.textContent = `${
-  //   Math.round((weatherData.main.temp - 273.15) * 10) / 10
-  // } ${currentDegrees}`;
-  // todayTemperature.classList.add("Celsius");
 
   toggleButton.textContent = "Display °F";
 
@@ -42,10 +21,12 @@ function createCurrentWeather(weatherData, forecastData) {
     forecastDays[day].childNodes[1].textContent = "";
     forecastDays[day].childNodes[1].appendChild(forecastImage);
     forecastImage.src = `http://openweathermap.org/img/wn/${forecastData.daily[day].weather[0].icon}@2x.png`;
+    
     forecastDays[day].childNodes[3].textContent = `${
-      Math.round(forecastData.daily[day].temp.day * 10) / 10
+      Math.round(forecastData.daily[day].temp.max * 10) / 10
     } ${currentDegrees}`;
     forecastDays[day].childNodes[3].classList.add("Celsius");
+
     forecastDays[day].childNodes[5].textContent = format(
       new Date(forecastData.daily[day].dt * 1000),
       "dd.MM.y"
@@ -58,7 +39,7 @@ function updateTodayWeather(weatherData, currentDegrees) {
   const todayCity = document.querySelector(".today__city");
   const todayImage = document.querySelector(".today__image");
 
-  const todayItsNow = document.querySelector(".additional__its-now");
+  const todayDescription = document.querySelector(".additional__description");
   const todayHumitidy = document.querySelector(".additional__humidity");
   const todayWindSpeed = document.querySelector(".additional__wind-speed");
 
@@ -67,7 +48,7 @@ function updateTodayWeather(weatherData, currentDegrees) {
   todayImage.textContent = "";
   todayImage.appendChild(weatherImage);
 
-  todayItsNow.textContent = `${weatherData.weather[0].description} now in ${weatherData.name}`;
+  todayDescription.textContent = `${weatherData.weather[0].description}`;
   todayCity.textContent = `${weatherData.name}, ${weatherData.sys.country}`;
   todayHumitidy.textContent = `Humidity: ${weatherData.main.humidity}%`;
   todayWindSpeed.textContent = `Wind speed: ${weatherData.wind.speed} m/s`;
@@ -89,17 +70,17 @@ function toggleDegrees(e) {
   const temperatureDivs = document.querySelectorAll(".temperature");
 
   for (let div of temperatureDivs) {
-    let thenum = div.textContent.replace(/[^\d.]/g, "");
+    let temperatureClean = div.textContent.replace(/[^\d.]/g, "");
     if (div.classList.contains("Farenheit")) {
       div.classList.remove("Farenheit");
       div.classList.add("Celsius");
       currentDegrees = "°C";
-      div.textContent = `${Math.round((thenum - 32) * (5 / 9) * 10) / 10} ${currentDegrees}`;
+      div.textContent = `${Math.round((temperatureClean - 32) * (5 / 9) * 10) / 10} ${currentDegrees}`;
     } else if (div.classList.contains("Celsius")) {
       div.classList.remove("Celsius");
       div.classList.add("Farenheit");
       currentDegrees = "°F";
-      div.textContent = `${Math.round((thenum * (9 / 5) + 32) * 10) / 10} ${currentDegrees}`;
+      div.textContent = `${Math.round((temperatureClean * (9 / 5) + 32) * 10) / 10} ${currentDegrees}`;
     }
   }
 }
