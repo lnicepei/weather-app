3f34d4565196b007b129073d566e9dea5be305d5
lnicepei <dmitrii.shaidorov@gmail.com>
2022-06-27 10:40:42 +0300

    Add IIFE to show the default city weather

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import { getWeather } from "./getWeather";
 
-document.addEventListener("keypress", (e) => {
+document.addEventListener("keypress", listenToDocument);
+
+function listenToDocument(e) {
   if (e.key === "Enter") getWeather();
-});
+  this.removeEventListener("click", listenToDocument);
+}
+
+(function autoWeather() {
+  getWeather();
+  document.querySelector(".toggle-temp").removeEventListener("click", getWeather);
+})();
