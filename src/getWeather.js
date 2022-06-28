fb2c571bc4a6400d9cc81a6bf73c9e963be51472
lnicepei <dmitrii.shaidorov@gmail.com>
2022-06-28 09:38:43 +0300

    Show "loading.." while loading

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,7 +1,9 @@
-import { createCurrentWeather } from "./updateWeatherDOM";
+import { createCurrentWeather, loading, unloading } from "./updateWeatherDOM";
 
 async function getWeather(city) {
+  loading();
   try {
+
     const unfecthedWeatherData = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=9d2f4030cf37634f60b9012e5bd611be`,
       { mode: "cors" }
@@ -15,9 +17,10 @@ async function getWeather(city) {
     );
 
     const forecastData = await unfetchedForecast.json();
-
+    unloading();
     createCurrentWeather(weatherData, forecastData);
   } catch (error) {
+    unloading();
     console.log(error);
     alert("Please enter an existing city");
   }
